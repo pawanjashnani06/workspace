d2da22c99b1a04c3033118132476e4c6b82c9678
Maciej Matys <maciej.matys@intive.com>
2017-06-27 16:49:27 +0200

    Init project based on spring initializer

---
+++ src/main/java/za/co/nedbank/bankingapiproxy/BankingApiProxyApplication.java
@@ -0,0 +1,12 @@
+package za.co.nedbank.bankingapiproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApiProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApiProxyApplication.class, args);
+    }
+}
---
+++ src/test/java/za/co/nedbank/bankingapiproxy/BankingApiProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.nedbank.bankingapiproxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BankingApiProxyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
